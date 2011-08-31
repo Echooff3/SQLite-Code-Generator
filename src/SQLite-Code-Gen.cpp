9aa4aef47ccf1cfacd512240f0e3421d4998ef93
Echooff3 <john.skibicki.iii@gmail.com>
2011-08-30 23:59:53 -0500

    Startup Project

---
+++ src/SQLite-Code-Gen.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SQLite-Code-Gen.cpp
+// Author      : JSkibicki
+// Version     :
+// Copyright   : 2011
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
